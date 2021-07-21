f22e5140d454438e24f8b927995379af8bd19000
minusInfinite <ashley@minusinfinite.net>
2021-07-21 22:03:30 +0930

    More functions and a test bed ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 {
-    var ytAppId = localStorage.getItem("ytapikey")
-    var ytApiUrl = "https://www.googleapis.com/youtube/v3/search"
-    var genAppToken = localStorage.getItem("genAppToken") //
+    var ytAppToken =
+        localStorage.getItem("ytAppToken") ||
+        "AIzaSyAqIufU5EeI9fBAs31i-vFXoJygeTTrBxQ"
+    var genAppToken =
+        localStorage.getItem("genAppToken") ||
+        "iKYR9jLVV-7Vuxd_ssoepsT6uh8h8Zjxcy_7i2Pyg-GU3t5DFegCdf80hQcSzodT"
     var genApiUrl = "https://api.genius.com/search/?q="
+    var ytVidUrl = "https://www.youtube.com/watch?v="
 
     var output = "../../output.json"
 
     var searchInput = document.querySelector("#search-bar")
     var searchBtn = document.querySelector("search-button")
 
-    var songs = []
+    function loadClient() {
+        gapi.client.setApiKey(`${ytAppToken}`)
+        return gapi.client
+            .load(
+                "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+            )
+            .then(
+                function () {
+                    console.log("GAPI client loaded for API")
+                },
+                function (err) {
+                    console.error("Error loading GAPI client for API", err)
+                }
+            )
+    }
 
     var getSongs = async function (value) {
+        var songs = []
         fetch(`${genApiUrl}${value}&access_token=${genAppToken}`)
             .then((res) => {
                 if (!res.ok) {
@@ -20,21 +39,35 @@
                 return res.json()
             })
             .then((data) => {
-                var title = data.hits[i].result.full_title
-                var img = data.hits[i].result.song_art_image_thumbnail_url
-                songs.push(title, img)
+                for (var i = 0; i < 3; i++) {
+                    songs.push(
+                        data.response.hits[i].results.full_title,
+                        data.response.hits[i].results.song_art_image_url
+                    )
+                }
             })
             .catch((err) => {
                 return err
             })
+        return { songs }
     }
 
-    var getQuery = function () {
-        var searchArr = document.location.search.split("&")
-
-        var query = searchArr[0].split("=").pop()
-
-        getSongs(query)
+    var getVideos = async function (value) {
+        return gapi.client.youtube.search
+            .list({
+                part: ["snippet"],
+                maxResults: 1,
+                q: `${value}`,
+            })
+            .then(
+                function (response) {
+                    // Handle the results here (response.result has the parsed body).
+                    console.log("Response", response)
+                },
+                function (err) {
+                    console.error("Execute error", err)
+                }
+            )
     }
 
     /* Create a DOMString */
@@ -51,5 +84,11 @@
         return el
     }
 
-    function pharseSongs() {}
+    var getQuery = function () {
+        var searchArr = document.location.search.split("&")
+
+        var query = searchArr[0].split("=").pop()
+
+        getSongs(query).then()
+    }
 }
