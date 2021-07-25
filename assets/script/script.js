7ae81e8293725e20669ecca49a409f7c8ec70d8f
Ashley <ashley@minusinfinite.net>
2021-07-25 12:32:47 +0930

    Removing

--- assets/script/script.js
+++
@@ -1,6 +0,0 @@
-var modalAppear = document.querySelector("#btn-modal");
-
-modalAppear.addEventListener('click', function() {
-    var modalTwo = Bulma('#mymodal').modal();
-    modalTwo.open();
-});
